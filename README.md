ca7557c39abb5fd7f9bdc5fa6f473092be639509
Arthur Nunes <coletivosuburbano@gmail.com>
2021-04-15 18:09:39 -0300

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,127 @@
+**Apparently the official creator was no longer active in the community, so I decided to make this modified version. If you are interested in suggesting, contributing or implementing something, feel free to do a PR.** _(Credits at the end.)_
+
+# Installation
+
+1. Navigate to your app directory where you want to include this style configuration.
+
+   ```bash
+   cd my-app
+   ```
+
+2. Run this command inside your app's root directory. Note: this command executes the `eslint-prettier-config.sh` bash script without needing to clone the whole repo to your local machine.
+
+   ```bash
+   exec 3<&1;bash <&3 <(curl https://raw.githubusercontent.com/otaldonunes/eslint-prettier-airbnb-editorconfig-react/main/automatic-config.sh 2> /dev/null)
+   ```
+
+3. Make selections for your preference of package manager (npm or yarn), file format (.js or .json), max-line size, and trailing commas (none, es5, all).
+
+4. Look in your project's root directory and notice the two newly added/updated config files:
+   - `.eslintrc.js` (or `.eslintrc.json`)
+   - `.prettierrc.js` (or `.prettierrc.json`)
+
+# Packages
+
+### Main Packages
+
+1. [ESlint](https://eslint.org/)
+2. [Prettier](https://prettier.io/)
+
+### Airbnb Configuration
+
+1. [eslint-config-airbnb](https://www.npmjs.com/package/eslint-config-airbnb)
+   - This package provides Airbnb's .eslintrc as an extensible shared config.
+2. [eslint-plugin-jsx-a11y](https://github.com/evcohen/eslint-plugin-jsx-a11y) (Peer Dependency)
+   - Static AST checker for accessibility rules on JSX elements.
+3. [eslint-plugin-react](https://github.com/yannickcr/eslint-plugin-react) (Peer Dependency)
+   - React specific linting rules for ESLint
+4. [eslint-plugin-import](https://www.npmjs.com/package/eslint-plugin-import) (Peer Dependency)
+   - Support linting of ES2015+ (ES6+) import/export syntax, and prevent issues with misspelling of file paths and import names.
+5. [babel-eslint](https://github.com/babel/babel-eslint)
+   - A wrapper for Babel's parser used for ESLint.
+   - We decided to include this since [Airbnb Style Guide uses Babel](https://github.com/airbnb/javascript#airbnb-javascript-style-guide-).
+
+### ESlint, Prettier Integration
+
+1. [eslint-plugin-prettier](https://github.com/prettier/eslint-plugin-prettier)
+   - Runs Prettier as an ESLint rule and reports differences as individual ESLint issues.
+2. [eslint-config-prettier](https://github.com/prettier/eslint-config-prettier)
+   - Turns off all rules that are unnecessary or might conflict with Prettier.
+
+# Created Configuration Files
+
+Once files are created, you may edit to your liking.
+
+### eslintrc(.js/.json)
+
+- [more info](https://eslint.org/docs/user-guide/configuring)
+
+```
+{
+"extends": [
+    "airbnb",
+    "plugin:prettier/recommended",
+    "prettier/react"
+  ],
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "jest": true,
+    "node": true
+  },
+  "rules": {
+    "jsx-a11y/href-no-hash": ["off"],
+    "react/jsx-filename-extension": ["warn", { "extensions": [".js", ".jsx"] }],
+    "max-len": [
+      "warn",
+      {
+        "code": (SET BY USER),
+        "tabWidth": 2,
+        "comments": (SET BY USER),
+        "ignoreComments": false,
+        "ignoreTrailingComments": true,
+        "ignoreUrls": true,
+        "ignoreStrings": true,
+        "ignoreTemplateLiterals": true,
+        "ignoreRegExpLiterals": true
+      }
+    ]
+  }
+}
+```
+
+### prettierrc(.js/.json)
+
+- [more Info](https://prettier.io/docs/en/configuration.html)
+
+```
+{
+  "printWidth": (SET BY USER),
+  "singleQuote": true,
+  "trailingComma": (SET BY USER)
+}
+```
+
+### .editorconfig
+
+- [more Info](https://editorconfig.org/#example-file)
+
+```
+root = true
+  [*.{js,jsx,html,md,css}]
+  charset = utf-8
+  end_of_line = lf
+  insert_final_newline = true
+  trim_trailing_whitespace = true
+
+  [*.{js,jsx,css}]
+  indent_size = 2
+  indent_style = space
+```
+
+---
+
+This script was created by [Paulo Ramos](https://github.com/paulolramos),
+Inspired by [Jeffrey Zhen's] [tutorial](https://blog.echobind.com/integrating-prettier-eslint-airbnb-style-guide-in-vscode-47f07b5d7d6a).
+Updated and modified with new features added by: [Arthur Nunes](https://github.com/otaldonunes), [Martin Dobrev](https://github.com/RAMTO), [Darshan Gada](https://github.com/dr5hn) and [Imed Adel](https://github.com/ImedAdel/).
---
+++ automatic-config.sh
@@ -0,0 +1,192 @@
+#!/bin/bash
+
+# ----------------------
+# Color Variables
+# ----------------------
+RED="\033[0;31m"
+YELLOW='\033[1;33m'
+GREEN='\033[1;32m'
+LCYAN='\033[1;36m'
+NC='\033[0m' # No Color
+
+# --------------------------------------
+# Prompts for configuration preferences
+# --------------------------------------
+
+# Package Manager Prompt
+echo
+echo "Which package manager are you using?"
+select package_command_choices in "Yarn" "Yarn Workspaces" "npm" "Cancel"; do
+  case $package_command_choices in
+    Yarn ) pkg_cmd='yarn add'; break;;
+    Yarn Workspaces ) pkg_cmd='yarn add -W'; break;;
+    npm ) pkg_cmd='npm install'; break;;
+    Cancel ) exit;;
+  esac
+done
+echo
+
+# File Format Prompt
+echo "Which ESLint and Prettier configuration format do you prefer?"
+select config_extension in ".js" ".json" "Cancel"; do
+  case $config_extension in
+    .js ) config_opening='module.exports = {'; break;;
+    .json ) config_opening='{'; break;;
+    Cancel ) exit;;
+  esac
+done
+echo
+
+# Checks for existing eslintrc files
+if [ -f ".eslintrc.js" -o -f ".eslintrc.yaml" -o -f ".eslintrc.yml" -o -f ".eslintrc.json" -o -f ".eslintrc" ]; then
+  echo -e "${RED}Existing ESLint config file(s) found:${NC}"
+  ls -a .eslint* | xargs -n 1 basename
+  echo
+  echo -e "${RED}CAUTION:${NC} there is loading priority when more than one config file is present: https://eslint.org/docs/user-guide/configuring#configuration-file-formats"
+  echo
+  read -p  "Write .eslintrc${config_extension} (Y/n)? "
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo -e "${YELLOW}>>>>> Skipping ESLint config${NC}"
+    skip_eslint_setup="true"
+  fi
+fi
+finished=false
+
+# Max Line Length Prompt
+while ! $finished; do
+  read -p "What max line length do you want to set for ESLint and Prettier? (Default Recommendation: 80)"
+  if [[ $REPLY =~ ^[0-9]{2,3}$ ]]; then
+    max_len_val=$REPLY
+    finished=true
+    echo
+  else
+    echo -e "${YELLOW}Setting default value to 80${NC}"
+    max_len_val=80
+  fi
+    finished=true
+    echo
+done
+
+# Trailing Commas Prompt
+echo "What style of trailing commas do you want to enforce with Prettier?"
+echo -e "${YELLOW}>>>>> See https://prettier.io/docs/en/options.html#trailing-commas for more details.${NC}"
+select trailing_comma_pref in "none" "es5" "all"; do
+  case $trailing_comma_pref in
+    none ) break;;
+    es5 ) break;;
+    all ) break;;
+  esac
+done
+echo
+
+# Checks for existing prettierrc files
+if [ -f ".prettierrc.js" -o -f "prettier.config.js" -o -f ".prettierrc.yaml" -o -f ".prettierrc.yml" -o -f ".prettierrc.json" -o -f ".prettierrc.toml" -o -f ".prettierrc" ]; then
+  echo -e "${RED}Existing Prettier config file(s) found${NC}"
+  ls -a | grep "prettier*" | xargs -n 1 basename
+  echo
+  echo -e "${RED}CAUTION:${NC} The configuration file will be resolved starting from the location of the file being formatted, and searching up the file tree until a config file is (or isn't) found. https://prettier.io/docs/en/configuration.html"
+  echo
+  read -p  "Write .prettierrc${config_extension} (Y/n)? "
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo -e "${YELLOW}>>>>> Skipping Prettier config${NC}"
+    skip_prettier_setup="true"
+  fi
+  echo
+fi
+
+# ----------------------
+# Perform Configuration
+# ----------------------
+echo
+echo -e "${GREEN}Configuring your development environment... ${NC}"
+
+echo
+echo -e "1/6 ${LCYAN}ESLint & Prettier Installation... ${NC}"
+echo
+$pkg_cmd -D eslint prettier
+
+echo
+echo -e "2/6 ${YELLOW}Conforming to Airbnb's JavaScript Style Guide... ${NC}"
+echo
+$pkg_cmd -D eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-import eslint-plugin-react babel-eslint
+
+echo
+echo -e "3/6 ${LCYAN}Making ESlint and Prettier play nice with each other... ${NC}"
+echo "See https://github.com/prettier/eslint-config-prettier for more details."
+echo
+$pkg_cmd -D eslint-config-prettier eslint-plugin-prettier
+
+
+if [ "$skip_eslint_setup" == "true" ]; then
+  break
+else
+  echo
+  echo -e "4/6 ${YELLOW}Building your .eslintrc${config_extension} file...${NC}"
+  > ".eslintrc${config_extension}" # truncates existing file (or creates empty)
+
+  echo ${config_opening}'
+  "extends": [
+    "airbnb",
+    "plugin:prettier/recommended",
+  ],
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "jest": true,
+    "node": true
+  },
+  "rules": {
+    "jsx-a11y/href-no-hash": ["off"],
+    "react/jsx-filename-extension": ["warn", { "extensions": [".js", ".jsx"] }],
+    "max-len": [
+      "warn",
+      {
+        "code": '${max_len_val}',
+        "tabWidth": 2,
+        "comments": '${max_len_val}',
+        "ignoreComments": false,
+        "ignoreTrailingComments": true,
+        "ignoreUrls": true,
+        "ignoreStrings": true,
+        "ignoreTemplateLiterals": true,
+        "ignoreRegExpLiterals": true
+      }
+    ]
+  }
+}' >> .eslintrc${config_extension}
+fi
+
+
+if [ "$skip_prettier_setup" == "true" ]; then
+  break
+else
+  echo -e "5/6 ${YELLOW}Building your .prettierrc${config_extension} file... ${NC}"
+  > .prettierrc${config_extension} # truncates existing file (or creates empty)
+
+  echo ${config_opening}'
+  "printWidth": '${max_len_val}',
+  "singleQuote": true,
+  "trailingComma": "'${trailing_comma_pref}'"
+}' >> .prettierrc${config_extension}
+fi
+
+echo -e "6/6 ${YELLOW}Building your .editorconfig file... ${NC}"
+  > .editorconfig # truncates existing file (or creates empty)
+
+  echo '
+  root = true
+  [*.{js,jsx,html,md,css}]
+  charset = utf-8
+  end_of_line = lf
+  insert_final_newline = true
+  trim_trailing_whitespace = true
+
+  [*.{js,jsx,css}]
+  indent_size = 2
+  indent_style = space
+' >> .editorconfig
+
+echo
+echo -e "${GREEN}Finished setting up!${NC}"
+echo
